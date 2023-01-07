fa6f1e70c220e9e09b6e3aae55a38f89a85596a6
JensRijks <jens.rijks123@gmail.com>
2023-01-07 21:28:06 +0100

    feat: added a second model

--- src/pages/MailAiPage/MailAI.tsx
+++ src/pages/MailAiPage/MailAI.tsx
@@ -1,7 +1,7 @@
 // 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
 import * as React from 'react';
 
-const API_KEY = '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';
+const API_KEY = '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';
 
 interface TextConverterProps {}
 
@@ -47,7 +47,26 @@ const TextConverter: React.FC<TextConverterProps> = () => {
             setOutputText(response.data.choices[0].text);
             console.log(response);
         } catch (error) {
-            console.error(error);
+            console.log('text-davinci-003 isnt working, trying text-ada-001');
+
+            try {
+                const { Configuration, OpenAIApi } = require('openai');
+                const configuration = new Configuration({
+                    apiKey: API_KEY
+                });
+                const openai = new OpenAIApi(configuration);
+                const response = await openai.createCompletion({
+                    model: 'text-ada-001',
+                    // prompt: `Kan je deze e-mail netter en meer professioneel maken in correct grammaticaal nederlands en aanheffing: "${inputText}"?`,
+                    prompt: `"${inputText}"`,
+                    temperature: 0.5,
+                    max_tokens: 1000
+                });
+                setOutputText(response.data.choices[0].text);
+                console.log(response);
+            } catch (error) {
+                console.error(error);
+            }
         }
     };
 
